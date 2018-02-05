5d253e8d9e72a5705313126b11b6744da53852df
clblamires <clblamires@lcsc.edu>
2018-02-05 13:28:35 -0800

    Adding missed files

---
+++ src/pages/class-schedule/class-schedule.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-class-schedule',
+  templateUrl: 'class-schedule.html'
+})
+export class ClassSchedulePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
