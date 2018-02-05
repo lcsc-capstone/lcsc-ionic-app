d452e62b9d0a9d6e2705402a0554bd801aca85e7
clblamires <clblamires@lcsc.edu>
2018-02-05 13:27:43 -0800

    Uploading the apps pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { ProfilePage } from '../pages/profile/profile';
+import { NewsPage } from '../pages/news/news';
+import { CampusMapPage } from '../pages/campus-map/campus-map';
+import { ClassSchedulePage } from '../pages/class-schedule/class-schedule';
+import { CalendarPage } from '../pages/calendar/calendar';
+import { AboutPage } from '../pages/about/about';
+import { RadioPage } from '../pages/radio/radio';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,17 +19,29 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ProfilePage,
+    NewsPage,
+    CampusMapPage,
+    ClassSchedulePage,
+    CalendarPage,
+    AboutPage,
+    RadioPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ProfilePage,
+    NewsPage,
+    CampusMapPage,
+    ClassSchedulePage,
+    CalendarPage,
+    AboutPage,
+    RadioPage
   ],
   providers: [
     StatusBar,
