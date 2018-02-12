fb090305fd85d87172b0aa44101e0c6da813a40f
luvjOi <CowgirlXena@gmail.com>
2018-02-12 12:33:50 -0800

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ClassSchedulePage } from '../class-schedule/class-schedule';
+import { CalendarPage } from '../calendar/calendar';
 
 @Component({
   selector: 'page-home',
@@ -13,5 +14,9 @@ export class HomePage {
   goToClassSchedule(params){
     if (!params) params = {};
     this.navCtrl.push(ClassSchedulePage);
+  }
+
+  gotoCalendarPage(){
+  this.navCtrl.push(CalendarPage);
   }
 }
