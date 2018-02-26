50ab67a8e9b4f9f965c1d5a980f411abc10b4424
Austin Larson <austinglarson@gmail.com>
2018-02-26 12:49:00 -0800

    Updated Login Homepage Design

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,73 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
 import { ClassSchedulePage } from '../class-schedule/class-schedule';
+import { CalendarPage } from '../calendar/calendar';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	public news = {'1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}, '9': {}, '10': {}};
 
-  constructor(public navCtrl: NavController) {
-  }
-  goToClassSchedule(params){
-    if (!params) params = {};
-    this.navCtrl.push(ClassSchedulePage);
-  }
+	constructor(public navCtrl: NavController, private http: Http, private storage: Storage) {
+	}
+	goToClassSchedule(params){
+		if (!params) params = {};
+		this.navCtrl.push(ClassSchedulePage);
+	}
+
+	gotoCalendarPage(){
+		this.navCtrl.push(CalendarPage);
+	}
+
+
+
+
+	ionViewDidLoad() {
+		let current_time = new Date().getTime();
+		let midnight = new Date(Math.floor(current_time/86400000)*86400000-57600000).getTime();
+		this.storage.get('last_time_news').then(val => {
+			if (val < midnight) {
+				// This code will fetch the most recent 3 news titles and links.
+				this.http.get(`http://www.lcsc.edu/news`).subscribe(data => {
+					let html = data['_body'];
+					let list = html.split(/<h4><a href="/g);
+					this.news['1']['link'] = list[1].split(/"/g)[0];
+					this.news['1']['title'] = list[1].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['2']['link'] = list[2];
+					this.news['2']['title'] = list[2].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['3']['link'] = list[3].split(/"/g)[0];
+					this.news['3']['title'] = list[3].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['4']['link'] = list[4].split(/"/g)[0];
+					this.news['4']['title'] = list[4].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['5']['link'] = list[5].split(/"/g)[0];
+					this.news['5']['title'] = list[5].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['6']['link'] = list[6].split(/"/g)[0];
+					this.news['6']['title'] = list[6].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['7']['link'] = list[7].split(/"/g)[0];
+					this.news['7']['title'] = list[7].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['8']['link'] = list[8].split(/"/g)[0];
+					this.news['8']['title'] = list[8].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['9']['link'] = list[9].split(/"/g)[0];
+					this.news['9']['title'] = list[9].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.news['10']['link'] = list[10].split(/"/g)[0];
+					this.news['10']['title'] = list[10].split(/title="/g)[1].split(/"/g)[0].replace(/&amp;/g, '&');
+					this.storage.set('news', this.news);
+					this.storage.set('last_time_news', current_time);
+				}, err => {
+					console.error(err)
+				}, () => {
+					console.log('getNews completed')
+				});
+			} else {
+				// This code will use the same news that is stored on the phone already.
+				this.storage.get('news').then(val => {
+					this.news = val;
+				});
+			}
+		});
+	}
 }
