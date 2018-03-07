e1150da20cbed0516f222562bab640b449cb13a4
Derry Everson <dneverson@lcmail.lcsc.edu>
2018-03-06 22:05:43 -0800

    Fixed a time error casuing crash in calendar tab, also being off by random hours.

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -36,11 +36,6 @@ export class CalendarPage {
 
 	ionViewDidLoad() {
 		this.storage.get('last_time').then(midnight => {
-			for (let i = 0; i < 62; i++) {
-				this.Events[Math.floor((midnight + i*86400000)/86400000).toString()] = {times: []};
-				this.Days.push(Math.floor((midnight + i*86400000)/86400000));
-			}
-
 			this.storage.get('Academics').then(val1 => {
 				this.Academics = val1;
 				this.storage.get('Entertainment').then(val2=> {
@@ -54,53 +49,57 @@ export class CalendarPage {
 								this.storage.get('CampusRec').then(val6 => {
 									this.CampusRec = val6;
 									let merged = [];
-									let offset = 32400000
+									let offset = 0
 
 									for (var i = 0; i < this.Academics.items.length; i++) {
-									var localdate = new Date(new Date((val1.items[i].start.dateTime || val1.items[i].start.date)));
-									var ls = new Date(new Date((val1.items[i].start.dateTime || val1.items[i].start.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var le = new Date(new Date((val1.items[i].end.dateTime || val1.items[i].end.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var localtime = (ls+" - "+le);
-									merged.push({StartDate:new Date((val1.items[i].start.dateTime || val1.items[i].start.date)).getTime()+offset, EndDate:new Date((val1.items[i].end.dateTime || val1.items[i].end.date)).getTime()+offset, Summary:val1.items[i].summary, Description:val1.items[i].description, Calendar:val1.summary, Location:val1.items[i].location, Link:val1.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
-								}
-								for (var i = 0; i < this.Entertainment.items.length; i++) {
-									var localdate = new Date(new Date((val2.items[i].start.dateTime || val2.items[i].start.date)));
-									var ls = new Date(new Date((val2.items[i].start.dateTime || val2.items[i].start.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var le = new Date(new Date((val2.items[i].end.dateTime || val2.items[i].end.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var localtime = (ls+" - "+le);
-									merged.push({StartDate:new Date((val2.items[i].start.dateTime || val2.items[i].start.date)).getTime()+offset, EndDate:new Date((val2.items[i].end.dateTime || val2.items[i].end.date)).getTime()+offset, Summary:val2.items[i].summary, Description:val2.items[i].description, Calendar:val2.summary, Location:val2.items[i].location, Link:val2.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
-								}
-								for (var i = 0; i < this.Athletics.items.length; i++) {
-									var localdate = new Date(new Date((val3.items[i].start.dateTime || val3.items[i].start.date)));
-									var ls = new Date(new Date((val3.items[i].start.dateTime || val3.items[i].start.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var le = new Date(new Date((val3.items[i].end.dateTime || val3.items[i].end.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var localtime = (ls+" - "+le);
-									merged.push({StartDate:new Date((val3.items[i].start.dateTime || val3.items[i].start.date)).getTime()+offset, EndDate:new Date((val3.items[i].end.dateTime || val3.items[i].end.date)).getTime()+offset, Summary:val3.items[i].summary, Description:val3.items[i].description, Calendar:val3.summary, Location:val3.items[i].location, Link:val3.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
-								}
-								for (var i = 0; i < this.StudentActivities.items.length; i++) {
-									var localdate = new Date(new Date((val4.items[i].start.dateTime || val4.items[i].start.date)));
-									var ls = new Date(new Date((val4.items[i].start.dateTime || val4.items[i].start.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var le = new Date(new Date((val4.items[i].end.dateTime || val4.items[i].end.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var localtime = (ls+" - "+le);
-									merged.push({StartDate:new Date((val4.items[i].start.dateTime || val4.items[i].start.date)).getTime()+offset, EndDate:new Date((val4.items[i].end.dateTime || val4.items[i].end.date)).getTime()+offset, Summary:val4.items[i].summary, Description:val4.items[i].description, Calendar:val4.summary, Location:val4.items[i].location, Link:val4.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
-								}
-								for (var i = 0; i < this.ResidentLife.items.length; i++) {
-									var localdate = new Date(new Date((val5.items[i].start.dateTime || val5.items[i].start.date)));
-									var ls = new Date(new Date((val5.items[i].start.dateTime || val5.items[i].start.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var le = new Date(new Date((val5.items[i].end.dateTime || val5.items[i].end.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var localtime = (ls+" - "+le);
-									merged.push({StartDate:new Date((val5.items[i].start.dateTime || val5.items[i].start.date)).getTime()+offset, EndDate:new Date((val5.items[i].end.dateTime || val5.items[i].end.date)).getTime()+offset, Summary:val5.items[i].summary, Description:val5.items[i].description, Calendar:val5.summary, Location:val5.items[i].location, Link:val5.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
-								}
-								for (var i = 0; i < this.CampusRec.items.length; i++) {
-									var localdate = new Date(new Date((val6.items[i].start.dateTime || val6.items[i].start.date)));
-									var ls = new Date(new Date((val6.items[i].start.dateTime || val6.items[i].start.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var le = new Date(new Date((val6.items[i].end.dateTime || val6.items[i].end.date)).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-									var localtime = (ls+" - "+le);
-									merged.push({StartDate:new Date((val6.items[i].start.dateTime || val6.items[i].start.date)).getTime()+offset, EndDate:new Date((val6.items[i].end.dateTime || val6.items[i].end.date)).getTime()+offset, Summary:val6.items[i].summary, Description:val6.items[i].description, Calendar:val6.summary, Location:val6.items[i].location, Link:val6.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
-								}
+										var localdate = new Date(new Date((val1.items[i].start.dateTime || val1.items[i].start.date + 'T00:00:00-08:00')));
+										var ls = new Date(new Date((val1.items[i].start.dateTime || val1.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var le = new Date(new Date((val1.items[i].end.dateTime || val1.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var localtime = (ls+" - "+le);
+										merged.push({StartDate:new Date((val1.items[i].start.dateTime || val1.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset, EndDate:new Date((val1.items[i].end.dateTime || val1.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:val1.items[i].summary, Description:val1.items[i].description, Calendar:val1.summary, Location:val1.items[i].location, Link:val1.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
+									}
+									for (var i = 0; i < this.Entertainment.items.length; i++) {
+										var localdate = new Date(new Date((val2.items[i].start.dateTime || val2.items[i].start.date + 'T00:00:00-08:00')));
+										var ls = new Date(new Date((val2.items[i].start.dateTime || val2.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var le = new Date(new Date((val2.items[i].end.dateTime || val2.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var localtime = (ls+" - "+le);
+										merged.push({StartDate:new Date((val2.items[i].start.dateTime || val2.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset, EndDate:new Date((val2.items[i].end.dateTime || val2.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:val2.items[i].summary, Description:val2.items[i].description, Calendar:val2.summary, Location:val2.items[i].location, Link:val2.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
+									}
+									for (var i = 0; i < this.Athletics.items.length; i++) {
+										var localdate = new Date(new Date((val3.items[i].start.dateTime || val3.items[i].start.date + 'T00:00:00-08:00')));
+										var ls = new Date(new Date((val3.items[i].start.dateTime || val3.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var le = new Date(new Date((val3.items[i].end.dateTime || val3.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var localtime = (ls+" - "+le);
+										merged.push({StartDate:new Date((val3.items[i].start.dateTime || val3.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset, EndDate:new Date((val3.items[i].end.dateTime || val3.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:val3.items[i].summary, Description:val3.items[i].description, Calendar:val3.summary, Location:val3.items[i].location, Link:val3.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
+									}
+									for (var i = 0; i < this.StudentActivities.items.length; i++) {
+										var localdate = new Date(new Date((val4.items[i].start.dateTime || val4.items[i].start.date + 'T00:00:00-08:00')));
+										var ls = new Date(new Date((val4.items[i].start.dateTime || val4.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var le = new Date(new Date((val4.items[i].end.dateTime || val4.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var localtime = (ls+" - "+le);
+										merged.push({StartDate:new Date((val4.items[i].start.dateTime || val4.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset, EndDate:new Date((val4.items[i].end.dateTime || val4.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:val4.items[i].summary, Description:val4.items[i].description, Calendar:val4.summary, Location:val4.items[i].location, Link:val4.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
+									}
+									for (var i = 0; i < this.ResidentLife.items.length; i++) {
+										var localdate = new Date(new Date((val5.items[i].start.dateTime || val5.items[i].start.date + 'T00:00:00-08:00')));
+										var ls = new Date(new Date((val5.items[i].start.dateTime || val5.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var le = new Date(new Date((val5.items[i].end.dateTime || val5.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var localtime = (ls+" - "+le);
+										merged.push({StartDate:new Date((val5.items[i].start.dateTime || val5.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset, EndDate:new Date((val5.items[i].end.dateTime || val5.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:val5.items[i].summary, Description:val5.items[i].description, Calendar:val5.summary, Location:val5.items[i].location, Link:val5.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
+									}
+									for (var i = 0; i < this.CampusRec.items.length; i++) {
+										var localdate = new Date(new Date((val6.items[i].start.dateTime || val6.items[i].start.date + 'T00:00:00-08:00')));
+										var ls = new Date(new Date((val6.items[i].start.dateTime || val6.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var le = new Date(new Date((val6.items[i].end.dateTime || val6.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+										var localtime = (ls+" - "+le);
+										merged.push({StartDate:new Date((val6.items[i].start.dateTime || val6.items[i].start.date + 'T00:00:00-08:00')).getTime()+offset, EndDate:new Date((val6.items[i].end.dateTime || val6.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:val6.items[i].summary, Description:val6.items[i].description, Calendar:val6.summary, Location:val6.items[i].location, Link:val6.items[i].htmlLink, LocalTime:localtime, LocalDate:localdate});
+									}
 									merged.sort(function(a,b){return a.StartDate - b.StartDate}).forEach(event => {
-										this.Events[Math.floor((event.StartDate/86400000))][event.Summary] = event;
-										this.Events[Math.floor((event.StartDate/86400000))]['times'].push(event.Summary);
+										if (!this.Events[Math.floor(((event.LocalDate.getTime()+16*3600000)/86400000))]) {
+											this.Events[Math.floor(((event.LocalDate.getTime()+16*3600000)/86400000))] = {times: []};
+											this.Days.push([Math.floor(((event.LocalDate.getTime()+16*3600000)/86400000))]);
+										}
+										this.Events[Math.floor(((event.LocalDate.getTime()+16*3600000)/86400000))][event.Summary] = event;
+										this.Events[Math.floor(((event.LocalDate.getTime()+16*3600000)/86400000))]['times'].push(event.Summary);
 									});
 								});
 							});
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -38,7 +38,8 @@ export class HomePage {
 		let current_time = new Date().getTime();
 		let midnight = new Date(Math.floor(current_time/86400000)*86400000-57600000).getTime();
 		this.storage.get('last_time').then(val => {
-			if (!val || val) { // TODO: change back to '<= midnight' once error checking is done
+
+    if (!val || val) { // TODO: change back to '<= midnight' once error checking is done
 				// This code will fetch the most recent 3 news titles and links.
 				this.storage.set('last_time', current_time);
 				this.http.get(`http://www.lcsc.edu/news`).subscribe(data => {
@@ -81,51 +82,48 @@ export class HomePage {
 				let curDay = (new Date().getDate());
 				let curMonth = (new Date().getMonth()+1); //For some reason we are pulling 1 month behind current month?
 				let curYear = (new Date().getFullYear());
-				let endDay = (new Date().getDate())
-				let endMonth = (new Date().getMonth()+3); // +3 to offset the +1 in curMonth and get 2 months adhead.
-				let endYear = (new Date().getFullYear());
 
-				this.http.get(`https://www.googleapis.com/calendar/v3/calendars/0rn5mgclnhc7htmh0ht0cc5pgk@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-07:00&timeMax=${endYear}-0${endMonth}-${endDay}T11:59:59-07:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
+				this.http.get(`https://www.googleapis.com/calendar/v3/calendars/0rn5mgclnhc7htmh0ht0cc5pgk@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-08:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
 					this.Academics = data.json();
 					this.storage.set('Academics', this.Academics);
-					this.http.get(`https://www.googleapis.com/calendar/v3/calendars/m6h2d5afcjfnmaj8qr7o96q89c@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-07:00&timeMax=${endYear}-0${endMonth}-${endDay}T11:59:59-07:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
+					this.http.get(`https://www.googleapis.com/calendar/v3/calendars/m6h2d5afcjfnmaj8qr7o96q89c@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-08:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
 						this.Entertainment = data.json();
 						this.storage.set('Entertainment', this.Entertainment);
-						this.http.get(`https://www.googleapis.com/calendar/v3/calendars/d6jbgjhudph2mpef1cguhn4g9g@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-07:00&timeMax=${endYear}-0${endMonth}-${endDay}T11:59:59-07:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
+						this.http.get(`https://www.googleapis.com/calendar/v3/calendars/d6jbgjhudph2mpef1cguhn4g9g@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-08:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
 							this.Athletics = data.json();
 							this.storage.set('Athletics', this.Athletics);
-							this.http.get(`https://www.googleapis.com/calendar/v3/calendars/l9qpkh5gb7dhjqv8nm0mn098fk@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-07:00&timeMax=${endYear}-0${endMonth}-${endDay}T11:59:59-07:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
+							this.http.get(`https://www.googleapis.com/calendar/v3/calendars/l9qpkh5gb7dhjqv8nm0mn098fk@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-08:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
 								this.StudentActivities = data.json();
 								this.storage.set('StudentActivities', this.StudentActivities);
-								this.http.get(`https://www.googleapis.com/calendar/v3/calendars/gqv0n6j15pppdh0t8adgc1n1ts@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-07:00&timeMax=${endYear}-0${endMonth}-${endDay}T11:59:59-07:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
+								this.http.get(`https://www.googleapis.com/calendar/v3/calendars/gqv0n6j15pppdh0t8adgc1n1ts@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-08:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
 									this.ResidentLife = data.json();
 									this.storage.set('ResidentLife', this.ResidentLife);
-									this.http.get(`https://www.googleapis.com/calendar/v3/calendars/h4j413d3q0uftb2crk0t92jjlc@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-07:00&timeMax=${endYear}-0${endMonth}-${endDay}T11:59:59-07:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
+									this.http.get(`https://www.googleapis.com/calendar/v3/calendars/h4j413d3q0uftb2crk0t92jjlc@group.calendar.google.com/events?maxResults=2500&timeMin=${curYear}-0${curMonth}-${curDay}T00:00:00-08:00&singleEvents=true&key=AIzaSyASiprsGk5LMBn1eCRZbupcnC1RluJl_q0`).subscribe(data => {
 										this.CampusRec = data.json();
 										this.storage.set('CampusRec', this.CampusRec);
 
 										//TODO: This needs changes. Badly. There is no reason to sort every event when only the first 3 are used. It is slow and very inefficient.
 										let merged = [];
 										let n = 0;
-										let offset = 28800001
+										let offset = 0
 
 										for (var i = 0; i < this.Academics.items.length; i++) {
-											merged.push({StartDate: new Date((this.Academics.items[i].start.dateTime || this.Academics.items[i].start.date)).getTime()+offset, EndDate: new Date((this.Academics.items[i].end.dateTime || this.Academics.items[i].end.date)).getTime()+offset, Summary:this.Academics.items[i].summary, Description:this.Academics.items[i].description});
+											merged.push({StartDate: new Date((this.Academics.items[i].start.dateTime || this.Academics.items[i].start.date + 'T00:00:00-08:01')).getTime()+offset, EndDate: new Date((this.Academics.items[i].end.dateTime || this.Academics.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:this.Academics.items[i].summary, Description:this.Academics.items[i].description});
 										}
 										for (var i = 0; i < this.Entertainment.items.length; i++) {
-											merged.push({StartDate: new Date((this.Entertainment.items[i].start.dateTime || this.Entertainment.items[i].start.date)).getTime()+offset, EndDate: new Date((this.Entertainment.items[i].end.dateTime || this.Entertainment.items[i].end.date)).getTime()+offset, Summary:this.Entertainment.items[i].summary, Description:this.Entertainment.items[i].description});
+											merged.push({StartDate: new Date((this.Entertainment.items[i].start.dateTime || this.Entertainment.items[i].start.date + 'T00:00:00-08:01')).getTime()+offset, EndDate: new Date((this.Entertainment.items[i].end.dateTime || this.Entertainment.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:this.Entertainment.items[i].summary, Description:this.Entertainment.items[i].description});
 										}
 										for (var i = 0; i < this.Athletics.items.length; i++) {
-											merged.push({StartDate: new Date((this.Athletics.items[i].start.dateTime || this.Athletics.items[i].start.date)).getTime()+offset, EndDate: new Date((this.Athletics.items[i].end.dateTime || this.Athletics.items[i].end.date)).getTime()+offset, Summary:this.Athletics.items[i].summary, Description:this.Athletics.items[i].description});
+											merged.push({StartDate: new Date((this.Athletics.items[i].start.dateTime || this.Athletics.items[i].start.date + 'T00:00:00-08:01')).getTime()+offset, EndDate: new Date((this.Athletics.items[i].end.dateTime || this.Athletics.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:this.Athletics.items[i].summary, Description:this.Athletics.items[i].description});
 										}
 										for (var i = 0; i < this.StudentActivities.items.length; i++) {
-											merged.push({StartDate: new Date((this.StudentActivities.items[i].start.dateTime || this.StudentActivities.items[i].start.date)).getTime()+offset, EndDate: new Date((this.StudentActivities.items[i].end.dateTime || this.StudentActivities.items[i].end.date)).getTime()+offset, Summary:this.StudentActivities.items[i].summary, Description:this.StudentActivities.items[i].description});
+											merged.push({StartDate: new Date((this.StudentActivities.items[i].start.dateTime || this.StudentActivities.items[i].start.date + 'T00:00:00-08:01')).getTime()+offset, EndDate: new Date((this.StudentActivities.items[i].end.dateTime || this.StudentActivities.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:this.StudentActivities.items[i].summary, Description:this.StudentActivities.items[i].description});
 										}
 										for (var i = 0; i < this.ResidentLife.items.length; i++) {
-											merged.push({StartDate: new Date((this.ResidentLife.items[i].start.dateTime || this.ResidentLife.items[i].start.date)).getTime()+offset, EndDate: new Date((this.ResidentLife.items[i].end.dateTime || this.ResidentLife.items[i].end.date)).getTime()+offset, Summary:this.ResidentLife.items[i].summary, Description:this.ResidentLife.items[i].description});
+											merged.push({StartDate: new Date((this.ResidentLife.items[i].start.dateTime || this.ResidentLife.items[i].start.date + 'T00:00:00-08:01')).getTime()+offset, EndDate: new Date((this.ResidentLife.items[i].end.dateTime || this.ResidentLife.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:this.ResidentLife.items[i].summary, Description:this.ResidentLife.items[i].description});
 										}
 										for (var i = 0; i < this.CampusRec.items.length; i++) {
-											merged.push({StartDate: new Date((this.CampusRec.items[i].start.dateTime || this.CampusRec.items[i].start.date)).getTime()+offset, EndDate: new Date((this.CampusRec.items[i].end.dateTime || this.CampusRec.items[i].end.date)).getTime()+offset, Summary:this.CampusRec.items[i].summary, Description:this.CampusRec.items[i].description});
+											merged.push({StartDate: new Date((this.CampusRec.items[i].start.dateTime || this.CampusRec.items[i].start.date + 'T00:00:00-08:01')).getTime()+offset, EndDate: new Date((this.CampusRec.items[i].end.dateTime || this.CampusRec.items[i].end.date + 'T00:00:00-08:00')).getTime()+offset, Summary:this.CampusRec.items[i].summary, Description:this.CampusRec.items[i].description});
 										}
 										merged.sort(function(a,b){return a.StartDate - b.StartDate}).forEach(event => {
 											if (n < 3) {
