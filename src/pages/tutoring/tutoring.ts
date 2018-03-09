c5b7398634b5411bf2c4d086c507dd317e9f7f07
crcahill <36085337+crcahill@users.noreply.github.com>
2018-03-09 11:48:14 -0800

    Deleted about page and added tutoring page

--- src/pages/tutoring/tutoring.ts
+++ src/pages/tutoring/tutoring.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-tutoring',
+  templateUrl: 'tutoring.html'
 })
-export class AboutPage {
+export class TutoringPage {
 
   constructor(public navCtrl: NavController) {
   }
