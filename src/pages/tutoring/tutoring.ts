12ef45de475723426fc9580aae81822a6908ca5e
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2018-03-16 11:28:19 -0700

    Testing papaparse csv parsing

--- src/pages/tutoring/tutoring.ts
+++ src/pages/tutoring/tutoring.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {HttpModule} from '@angular/http';
+import * as papa from 'papaparse';
 @Component({
   selector: 'page-tutoring',
   templateUrl: 'tutoring.html'
 })
-export class TutoringPage {
 
-  constructor(public navCtrl: NavController) {
-  }
-  
+export class TutoringPage {
+	public csvItems:any;
+
+	ClassList =[
+		 {
+		   name: '208'
+		 },
+		 {
+		   name: '111'
+		 }
+	];
+	TutorList =[
+		 {
+		   name: 'Jack'
+		 },
+		 {
+		   name: 'Jim'
+		 }
+	];
+private extractData() {
+	//let parsedData
+  this.TutorList= papa.parse("http://isoptera.lcsc.edu/~jamcdonald/tutors.csv",{
+		download:true,
+		complete:function(results){
+			console.log(results)
+		}
+	});
+  //this.TutorList=parsedData;
+	//for(let i=0;i<parsedData.;i++){
+  //this.TutorList.push({
+  //    name:parsedData[0][name]
+  //});
+  //}
+
+	}
+	constructor(public navCtrl: NavController){
+		this.extractData();
+	}
+
+
 }
