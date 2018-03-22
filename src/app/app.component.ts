365b15ce596ff8321bd005d9bb72b13b642daa96
Derry Everson <dneverson@lcmail.lcsc.edu>
2018-03-22 16:40:05 -0700

    Merge branch 'master' of https://github.com/lcsc-capstone/lcsc-ionic-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
-
 import { ClassSchedulePage } from '../pages/class-schedule/class-schedule';
 import { NewsPage } from '../pages/news/news';
 import { CampusMapPage } from '../pages/campus-map/campus-map';
@@ -11,11 +10,7 @@ import { AboutPage } from '../pages/about/about';
 import { RadioPage } from '../pages/radio/radio';
 import { CalendarPage } from '../pages/calendar/calendar';
 import { LoginPage } from '../pages/login/login';
-
-
 import { HomePage } from '../pages/home/home';
-
-
 
 @Component({
   templateUrl: 'app.html'
