74dbcbb4efca22c5ac3907a72a0da85788797035
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2018-04-02 11:05:00 -0700

    debug changes to add tutoring to menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { ClassSchedulePage } from '../pages/class-schedule/class-schedule';
 import { ProfilePage } from '../pages/profile/profile';
 import { NewsPage } from '../pages/news/news';
 import { CampusMapPage } from '../pages/campus-map/campus-map';
-import { AboutPage } from '../pages/about/about';
+import { TutoringPage } from '../pages/tutoring/tutoring';
 import { RadioPage } from '../pages/radio/radio';
 import { CalendarPage } from '../pages/calendar/calendar';
 import { LoginPage } from '../pages/login/login';
