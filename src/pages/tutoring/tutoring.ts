79c1d2fbeb091519c18b99c8e51c6a20f00e8950
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2018-04-11 11:44:50 -0700

    combinedd
    recover single file from server rather than 3

--- src/pages/tutoring/tutoring.ts
+++ src/pages/tutoring/tutoring.ts
@@ -11,6 +11,7 @@ export class TutoringPage {
   public classKey=""
   public tutorKey=""
   public Time=["Sample Time","Sample Time"];
+  public Times={"key":["time","time"]}
   public ListName=""
   public isSubmitted=false
   public isOnline=false
@@ -32,19 +33,26 @@ export class TutoringPage {
 extractData() {
 	//let parsedData
 
-  this.http.get('http://isoptera.lcsc.edu/~jamcdonald/classes.json', {}, {})/*.map(res => { res.json()})*/.then(data => {
+  this.http.get('http://isoptera.lcsc.edu/~jamcdonald/combined.json', {}, {})/*.map(res => { res.json()})*/.then(data => {
     //this.debug2 = JSON.parse(data['_body']).items[0].name;
-    this.ClassList= JSON.parse(data.data).items;
+    this.ClassList= JSON.parse(data.data)["classes"];
+    this.TutorList= JSON.parse(data.data)["tutors"];
+    this.Times= JSON.parse(data.data)["times"];
   });
 
 //  this.ClassList.push({
 //    name:"test"
 //  });
-  this.http.get('http://isoptera.lcsc.edu/~jamcdonald/tutors.json', {}, {})/*.map(res => { res.json()})*/.then(data => {
-    //this.debug2 = JSON.parse(data['_body']).items[0].name;
-    this.TutorList=JSON.parse(data.data).items;
-  });
 
+//  this.http.get('http://isoptera.lcsc.edu/~jamcdonald/tutors.json', {}, {})/*.map(res => { res.json()})*/.then(data => {
+//    //this.debug2 = JSON.parse(data['_body']).items[0].name;
+//    this.TutorList=JSON.parse(data.data).items;
+//  });
+//  this.http.get('http://isoptera.lcsc.edu/~jamcdonald/times.json', {}, {})/*.map(res => { res.json()})*/.then(data => {
+//    //this.debug2 = JSON.parse(data['_body']).items[0].name;
+//    this.Times=JSON.parse(data.data);
+//    alert(this.Times);
+//  });
 
 	}
   Submit( form: NgForm ){
@@ -56,7 +64,9 @@ extractData() {
     }
     this.classKey=""
     this.tutorKey=""
+    this.Time=this.Times[this.ListName]
     this.isSubmitted=true;
+
   //  form.value.class;
 }
 	constructor(public navCtrl: NavController, public http: HTTP){
