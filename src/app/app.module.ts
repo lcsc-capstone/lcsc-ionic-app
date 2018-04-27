98c559825926f4f87201bccb9acf4ef507a3ea15
Mikel Jensen <mrjensen@lcmail.lcsc.edu>
2018-04-27 11:02:37 -0700

    Changes made to make it build for production on android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,8 @@ import { NewsPage } from '../pages/news/news';
 import { CampusMapPage } from '../pages/campus-map/campus-map';
 import { ClassSchedulePage } from '../pages/class-schedule/class-schedule';
 import { CalendarPage } from '../pages/calendar/calendar';
-import { CalendarDropdownPage } from '../pages/calendar/calendar-dropdown';
 import { TutoringPage } from '../pages/tutoring/tutoring';
 import { RadioPage } from '../pages/radio/radio';
-import { LoginPage } from '../pages/login/login';
 import { BuildingHoursPage } from '../pages/building-hours/building-hours';
 import { BuildingDetailsPage } from '../pages/building-details/building-details';
 
@@ -38,10 +36,8 @@ import { AppAvailability } from '@ionic-native/app-availability';
 		CampusMapPage,
 		ClassSchedulePage,
 		CalendarPage,
-		CalendarDropdownPage,
 		TutoringPage,
 		RadioPage,
-		LoginPage,
 		BuildingHoursPage,
 		BuildingDetailsPage
 	],
@@ -79,10 +75,8 @@ import { AppAvailability } from '@ionic-native/app-availability';
 		CampusMapPage,
 		ClassSchedulePage,
 		CalendarPage,
-		CalendarDropdownPage,
 		TutoringPage,
 		RadioPage,
-		LoginPage,
 		BuildingHoursPage,
 		BuildingDetailsPage
 	],
--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -9,20 +9,6 @@ import { AlertController } from 'ionic-angular';
 	selector: 'page-calendar',
 	templateUrl: 'calendar.html'
 })
-
-@Component({
-	template: `
-	<ion-list>
-	<ion-list-header>Ionic</ion-list-header>
-	<button ion-item (click)="close()">Learn Ionic</button>
-	<button ion-item (click)="close()">Documentation</button>
-	<button ion-item (click)="close()">Showcase</button>
-	<button ion-item (click)="close()">GitHub Repo</button>
-	</ion-list>
-	`
-})
-
-@Component({})
 export class CalendarPage {
 	public Academics;
 	public Entertainment;
